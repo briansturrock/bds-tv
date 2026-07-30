376705a3ac9d615c1138c12d8cdcb4df8722b95f
Brian Sturrock <brian.sturrock@hpe.com>
2026-07-30 15:28:34 +0100

    Use explicit TV programme image spacing

--- tizen/bds-tv/main.js
+++ tizen/bds-tv/main.js
@@ -1,4 +1,4 @@
-var TV_SHELL_VERSION = "0.1.26";
+var TV_SHELL_VERSION = "0.1.27";
 var DEFAULT_SERVER = "http://192.168.0.185:8088";
 var SERVER_KEY = "bdsTvServerUrl";
 var GUIDE_WINDOW_HOURS = 2;
