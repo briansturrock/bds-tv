49d1ef4c9bee8e93a56cb4a9fe64a70a2dfc1ba5
Brian Sturrock <brian.sturrock@hpe.com>
2026-07-31 13:59:35 +0100

    Release 1.1.0 - Sonarr integration

--- tizen/bds-tv/main.js
+++ tizen/bds-tv/main.js
@@ -1,4 +1,4 @@
-var TV_SHELL_VERSION = "1.1.0-sonarr.11";
+var TV_SHELL_VERSION = "1.1.0";
 var DEFAULT_SERVER = "http://192.168.0.185:8088";
 var SERVER_KEY = "bdsTvServerUrl";
 var GUIDE_WINDOW_HOURS = 2.5;
